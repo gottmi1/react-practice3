544b29e16837a46091f0f14a3f871460e79bddd6
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-04-14 03:23:21 +0900

    유효한 값일 시 더이상 검증 안 하도록

--- pra3/src/components/Login/Login.js
+++ pra3/src/components/Login/Login.js
@@ -52,42 +52,44 @@ const Login = (props) => {
   //   return () => {
   //     console.log("이펙트러닝 클리너");
   //   };
-  // }, [enteredPassword]);
+  // }, []);
   // // 두번째 인자가 없으면 모든 사이드이펙트에 실행되고 빈 배열이면 렌더될 때 딱 한번 실행, 두번째 인자에 무언가 있다면 그것의 상태가 변할 때 마다 실행됨
 
-  // useEffect(() => {
-  //   // 그리고 useEffect는 함수도 리턴할 수 있다
-  //   const identifier = setTimeout(() => {
-  //     // 여기서 하고싶은 건 setFormIsValid의 조건을 만족하면 500ms 이후에 console.log()를 띄우는 것
-  //     console.log("상태감지중");
-  //     setFormIsValid(
-  //       enteredEmail.includes("@") && enteredPassword.trim().length > 6
-  //     );
-  //   }, 500);
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+  // 객체 디스트럭처링 구문의 별칭할당. 값만 변경되고 유효성은 변경되지 않으면 이 이펙트는 다시 실행되지 않는다.
+  // State들의 isValid가 이미 true일 떄 입력을 더 받더라도 쓸데없는 유효성 검사를 더 하지 않아도 되도록
 
-  //   return () => {
-  //     console.log("클린업");
-  //     clearTimeout(identifier);
-  //   };
-  //   // 요걸 클린업 함수라고 함 첫번째 사이드 이펙트가 실행된 후 마다 실행됨
-  //   // 입력이 있을 때마다 앞에 일어난 사이드 이펙트인 셋타임아웃을 클리어 해주고 500ms동안 상태 변화가 없으면 그건 클리어 안함
-  // }, [enteredEmail, enteredPassword]);
-  // // 배열을 비워두면 렌더링 될 때 딱 한번만 실행되지만, 배열에 요소를 넣어두면 해당 요소들 모두에 변화가 있을 때 setFormIsValid를 실행한다
-  // // 컴포넌트가 처음으로 렌더 되었을 때만이 아니라 상태나 프랍과 같은 데이터가 바뀌었을 때 로직을 다시 실행하는 것이 일반적인 사용법
+  useEffect(() => {
+    // 그리고 useEffect는 함수도 리턴할 수 있다
+    const identifier = setTimeout(() => {
+      // 여기서 하고싶은 건 setFormIsValid의 조건을 만족하면 500ms 이후에 console.log()를 띄우는 것
+      console.log("상태감지중");
+      setFormIsValid(emailIsValid && passwordIsValid);
+    }, 500);
+
+    return () => {
+      console.log("클린업");
+      clearTimeout(identifier);
+    };
+    // 요걸 클린업 함수라고 함 첫번째 사이드 이펙트가 실행된 후 마다 실행됨
+    // 입력이 있을 때마다 앞에 일어난 사이드 이펙트인 셋타임아웃을 클리어 해주고 500ms동안 상태 변화가 없으면 그건 클리어 안함
+  }, [emailIsValid, passwordIsValid]);
+  // 배열을 비워두면 렌더링 될 때 딱 한번만 실행되지만, 배열에 요소를 넣어두면 해당 요소들 모두에 변화가 있을 때 setFormIsValid를 실행한다
+  // 컴포넌트가 처음으로 렌더 되었을 때만이 아니라 상태나 프랍과 같은 데이터가 바뀌었을 때 로직을 다시 실행하는 것이 일반적인 사용법
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "user_input", val: event.target.value }); // type필드가 있는 객체를 액션이라고 함
 
-    setFormIsValid(
-      passwordState.isValid > 6 && event.target.value.includes("@")
-    );
+    // setFormIsValid(
+    //   passwordState.isValid > 6 && event.target.value.includes("@")
+    // );
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({ type: "password_input", val: event.target.value });
 
-    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
-    // isValid가 true일 때
+    // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6); // isValid가 true일 때
   };
 
   const validateEmailHandler = () => {
